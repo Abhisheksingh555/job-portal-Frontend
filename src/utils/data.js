8e8e59591eb1c1130c8a0d3aa72451f85a7fcaab
Abhishek Singh <136239184+Abhisheksingh555@users.noreply.github.com>
2025-11-09 04:25:11 +0530

    Change API endpoints to localhost for development

--- src/utils/data.js
+++ src/utils/data.js
@@ -1,4 +1,4 @@
-export const USER_API_ENDPOINT = "https://job-portal-backend-faf99iy9u-abhiis-projects-cb74ebe0.vercel.app/api/user";
-export const JOB_API_ENDPOINT = "https://job-portal-backend-faf99iy9u-abhiis-projects-cb74ebe0.vercel.app/api/job";
+export const USER_API_ENDPOINT = "http://localhost:5011/api/user";
+export const JOB_API_ENDPOINT = "http://localhost:5011/api/job";
 export const APPLICATION_API_ENDPOINT = "https://job-portal-backend-faf99iy9u-abhiis-projects-cb74ebe0.vercel.app/api/application";
 export const COMPANY_API_ENDPOINT = "https://job-portal-backend-faf99iy9u-abhiis-projects-cb74ebe0.vercel.app/api/company";
